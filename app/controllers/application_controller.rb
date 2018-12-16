7398545e11cfc3a1cace4441e96595171829fb96
Tammy Metz <tammy@tammymetz.com>
2018-12-16 18:50:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,17 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+  
+  get '/recipes/new' do
+    erb :new
+  end
 
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end
 end
